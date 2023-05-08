9b6200e4bae0c13433ad09409ab2a4ef6c8d1acb
maria <maria@LAPTOP-FDLPU394>
2023-05-08 16:30:43 +0300

    parandus

--- games-vue-front/src/views/userGamesView.vue
+++ games-vue-front/src/views/userGamesView.vue
@@ -4,13 +4,10 @@
 </template>
 <script>
 const API_URL = "http://localhost:8080/games"
-
-
 import userGames from "../components/userGames.vue"
 import gameDetails from "../components/gameDetails.vue"
 import gameList from "../components/gameList.vue"
 import gameForm from  "../components/gameForm.vue"
-
 export default {
     props: {
         playerId: Number,
